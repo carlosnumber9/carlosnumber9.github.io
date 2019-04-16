f1e4f1886b81773961ff5a7cd0b8dd32d6f37ff0
carlosnumber9 <charlie_theboss023@hotmail.com>
2019-04-16 20:10:00 +0200

    Primeros arreglos al despliegue de los apartados.

--- js/action.js
+++ js/action.js
@@ -34,7 +34,41 @@ $(document).ready(function() {
     }, 1200);
     
     setTimeout(function() { 
-        $('.cuadro *').fadeIn(1000);
+        $('.cuadro *').not('.apcontent').not('.hback').fadeIn(1000);
     }, 1750);
+
+
+    $('.apartado').mouseenter(function(){
+        $('body').css('cursor', 'pointer');
+        $(this).animate({'backgroundColor': "rgba(115,115,115,0.5)"}, 200);
+        $(this).children('.ddown').animate({opacity: 1}, 100);
+    });
+
+    $('.apartado').mouseleave(function(){
+        $('body').css('cursor', 'auto');
+        $(this).animate({'backgroundColor': "rgba(153,153,153,0.5)"}, 200);
+        $(this).children('.ddown').animate({opacity: 0}, 100);
+    });
+
+    $('.apartado.cerrado').click(function(){
+
+        $(this).removeClass('cerrado');
+        $(this).children('.hback').fadeIn(1000);
+        $(this).children('.ddown').animate({opacity: 0}, 100);
+        $(this).children('.dup').animate({opacity: 1}, 100);
+        
+        $(this).children('.apcontent').fadeIn(1000);
+    });
+
+
+    $('.hback').click(function(){
+        $(this).parent().children('.ddown').animate({opacity: 1}, 100);
+        $(this).parent().children('.dup').animate({opacity: 0}, 100);
+        $(this).parent().children().not('.ddown').fadeOut(1000);
+        $(this).parent().addClass('cerrado');
+    });
+
+
+
 
 })
