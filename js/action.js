45b3cce1c5bad2b6a6c32a33dfd19b318cff7a36
carlosnumber9 <charlie_theboss023@hotmail.com>
2019-04-17 03:02:14 +0200

    Objetivo mecánica de los apartados

--- js/action.js
+++ js/action.js
@@ -1,5 +1,23 @@
 
 
+function extenderDiv(){
+
+    let despTotal;
+
+    $(this).each(function(){
+        despTotal = 0;
+        $(this).children().each(function(){
+            despTotal = despTotal + $(this).height();
+        });
+    });
+
+    return despTotal;
+}
+
+
+
+
+
 $(document).ready(function() {
 
 
@@ -26,11 +44,15 @@ $(document).ready(function() {
 
     setTimeout(function() { 
         $('.cuadro').fadeIn(1000);
+        $('#tarjeta').fadeIn(1000);
         $('.mtext').fadeIn(1000);
     }, 1000);
 
     setTimeout(function() { 
-        $('.cuadro').animate({height: 'auto'}, 250);
+        $('#tarjeta').each().animate({height: $(this).extenderDiv()}, 250);
+        //$('.cuadro').animate({height: 'auto'}, 250);
+        
+        //$('.cuadro').animate({height: 'auto'}, 250);
     }, 1200);
     
     setTimeout(function() { 
@@ -41,19 +63,56 @@ $(document).ready(function() {
     $('.apartado').mouseenter(function(){
         $('body').css('cursor', 'pointer');
         $(this).animate({'backgroundColor': "rgba(115,115,115,0.5)"}, 200);
-        $(this).children('.ddown').animate({opacity: 1}, 100);
+        if($(this).hasClass('cerrado')) $(this).children('.ddown').animate({opacity: 1}, 100);
+        else $(this).children('.dup').animate({opacity: 1}, 100);
     });
 
     $('.apartado').mouseleave(function(){
         $('body').css('cursor', 'auto');
         $(this).animate({'backgroundColor': "rgba(153,153,153,0.5)"}, 200);
-        $(this).children('.ddown').animate({opacity: 0}, 100);
+        if($(this).hasClass('cerrado')) $(this).children('.ddown').animate({opacity: 0}, 100);
+        else $(this).children('.dup').animate({opacity: 0}, 100);
     });
 
+
+    $('.apartado').click(function(){
+        if($(this).hasClass('cerrado')){
+
+            //$(this).animate({height: $(this).extenderDiv()}, 250);
+
+            $(this).children('.hback').fadeIn(1000);
+
+            $(this).children('.ddown').animate({opacity: 0}, 100);
+            $(this).children('.dup').animate({opacity: 1}, 100);
+        
+            
+
+            $(this).children('.apcontent').fadeIn(1000);
+        }
+        else {
+            $(this).children('.ddown').animate({opacity: 1}, 100);
+            $(this).children('.dup').animate({opacity: 0}, 100);
+    
+            $(this).children().not('.ddown').fadeOut(1000);
+            
+            
+            $(this).animate({height: ''}, 250);
+            
+            
+        }
+
+        $(this).toggleClass('cerrado'); 
+    });
+
+
+
+/*
     $('.apartado.cerrado').click(function(){
 
-        $(this).removeClass('cerrado');
+        $(this).toggleClass('cerrado');
+
         $(this).children('.hback').fadeIn(1000);
+
         $(this).children('.ddown').animate({opacity: 0}, 100);
         $(this).children('.dup').animate({opacity: 1}, 100);
         
@@ -62,13 +121,20 @@ $(document).ready(function() {
 
 
     $('.hback').click(function(){
+
         $(this).parent().children('.ddown').animate({opacity: 1}, 100);
         $(this).parent().children('.dup').animate({opacity: 0}, 100);
-        $(this).parent().children().not('.ddown').fadeOut(1000);
-        $(this).parent().addClass('cerrado');
+
+        setTimeout(function(){
+            $(this).parent().children().not('.ddown').fadeOut(1000);
+        }, 500);
+        
+        $(this).parent().each().animate({height: '40px'}, 250);
+        
+        $(this).parent().toggleClass('cerrado');
     });
+*/
 
 
 
-
-})
+});
