47564b351b403f530151f2a920e84d6a86ab8abf
carlosnumber9 <charlie_theboss023@hotmail.com>
2019-04-20 00:25:17 +0200

    Optimización. Dinamismo de la tarjeta

--- js/action.js
+++ js/action.js
@@ -1,17 +1,42 @@
 
 
-function extenderDiv(){
+function extenderDiv(selector){
 
+    /*
     let despTotal;
 
-    $(this).each(function(){
+    selector.each(function(){
         despTotal = 0;
-        $(this).children().each(function(){
-            despTotal = despTotal + $(this).height();
+        let hijo = $(this);
+        hijo.children().each(function(){
+
+            if($(this).is('a')) {
+                console.log("Encontrado un link con longitud " + extenderDiv($(this)));
+
+                despTotal = despTotal + extenderDiv($(this));  
+            }
+            else {
+                despTotal = despTotal + $(this).outerHeight();
+                console.log("Elemento con longitud " + $(this).outerHeight());
+            }
+
+            
+            
         });
     });
 
     return despTotal;
+    */
+   totalHeight = 0;
+   selector.children().each(function(){
+        totalHeight += $(this).outerHeight(true); // true = include margins
+    });
+    console.log("Total Height: " + totalHeight + "px");
+
+    return totalHeight;
+
+
+
 }
 
 
@@ -20,88 +45,87 @@ function extenderDiv(){
 
 $(document).ready(function() {
 
+    retrato = $('#retrato');
+    nombre = $('#nombre');
+    titulo = $('#estudiante');
+    tarjeta = $('#tarjeta');
+    
+    initHeight1 = '40px';
+    expHeight = extenderDiv(tarjeta);
 
-        
-    //$('#bground').addClass("bgBlur"); 
 
-    /*
     setTimeout(function() { 
-        $('.cabecera').fadeIn(1000);
-    }, 1500);
-    */
-
-    setTimeout(function() { 
-        $('#retrato').fadeIn(1000);
+        retrato.fadeIn(1000);
     }, 500);
 
     setTimeout(function() { 
-        $('#nombre').fadeIn(1000);
+        nombre.fadeIn(1000);
     }, 750);
 
     setTimeout(function() { 
-        $('#estudiante').fadeIn(1000);
+        titulo.fadeIn(1000);
     }, 875);
 
     setTimeout(function() { 
         $('.cuadro').fadeIn(1000);
-        $('#tarjeta').fadeIn(1000);
+        tarjeta.fadeIn(500);
         $('.mtext').fadeIn(1000);
-    }, 1000);
+    }, 900);
 
     setTimeout(function() { 
-        $('#tarjeta').each().animate({height: $(this).extenderDiv()}, 250);
-        //$('.cuadro').animate({height: 'auto'}, 250);
-        
-        //$('.cuadro').animate({height: 'auto'}, 250);
+        tarjeta.animate({height: extenderDiv(tarjeta)}, 250);
     }, 1200);
     
     setTimeout(function() { 
+        tarjeta.find('*').animate({opacity: 1}, 250);
         $('.cuadro *').not('.apcontent').not('.hback').fadeIn(1000);
     }, 1750);
 
 
     $('.apartado').mouseenter(function(){
+        let apartado = $(this);
+
         $('body').css('cursor', 'pointer');
-        $(this).animate({'backgroundColor': "rgba(115,115,115,0.5)"}, 200);
-        if($(this).hasClass('cerrado')) $(this).children('.ddown').animate({opacity: 1}, 100);
-        else $(this).children('.dup').animate({opacity: 1}, 100);
+        apartado.animate({'backgroundColor': "rgba(115,115,115,0.5)"}, 200);
+        if(apartado.hasClass('cerrado')) apartado.children('.ddown').animate({opacity: 1}, 100);
+        else apartado.children('.dup').animate({opacity: 1}, 100);
     });
 
     $('.apartado').mouseleave(function(){
+        let apartado = $(this);
+
         $('body').css('cursor', 'auto');
-        $(this).animate({'backgroundColor': "rgba(153,153,153,0.5)"}, 200);
-        if($(this).hasClass('cerrado')) $(this).children('.ddown').animate({opacity: 0}, 100);
-        else $(this).children('.dup').animate({opacity: 0}, 100);
+        apartado.animate({'backgroundColor': "rgba(153,153,153,0.5)"}, 200);
+        if(apartado.hasClass('cerrado')) apartado.children('.ddown').animate({opacity: 0}, 100);
+        else apartado.children('.dup').animate({opacity: 0}, 100);
     });
 
 
     $('.apartado').click(function(){
-        if($(this).hasClass('cerrado')){
+        let apartado = $(this);
 
-            //$(this).animate({height: $(this).extenderDiv()}, 250);
+        if(apartado.hasClass('cerrado')){
 
-            $(this).children('.hback').fadeIn(1000);
+            apartado.children('.hback').fadeIn(1000);
+            apartado.children('.ddown').animate({opacity: 0}, 100);
+            apartado.children('.dup').animate({opacity: 1}, 100);
 
-            $(this).children('.ddown').animate({opacity: 0}, 100);
-            $(this).children('.dup').animate({opacity: 1}, 100);
-        
-            
+            apartado.animate({height: extenderDiv()}, 250);
 
-            $(this).children('.apcontent').fadeIn(1000);
+            apartado.children('.apcontent').fadeIn(1000);
+
         }
         else {
-            $(this).children('.ddown').animate({opacity: 1}, 100);
-            $(this).children('.dup').animate({opacity: 0}, 100);
-    
-            $(this).children().not('.ddown').fadeOut(1000);
-            
-            
-            $(this).animate({height: ''}, 250);
+
+            apartado.children('.ddown').animate({opacity: 1}, 100);
+            apartado.children('.dup').animate({opacity: 0}, 100);
+            apartado.children().not('.ddown').fadeOut(1000);
+            apartado.animate({height: ''}, 250);
             
             
         }
 
-        $(this).toggleClass('cerrado'); 
+        apartado.toggleClass('cerrado'); 
     });
 
 
