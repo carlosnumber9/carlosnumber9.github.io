a0bcdd03a5501f0fdcbbfb1ac939710557aff49d
carlosnumber9 <charlie_theboss023@hotmail.com>
2020-09-05 20:12:53 +0200

    First version for interaction between navigation bar buttons and sections

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 
+var SECTIONS = {
+    INTRODUCTION: 'introduction',
+    CAREER_GOALS: 'career-goals',
+    ACADEMIC_BACKGROUND: 'academic-background',
+    EXPERIENCE: 'experience',
+    SKILLS: 'skills',
+    PROJECTS: 'projects',
+}
+
+var selectedSection;
+
 function initPresentationCard() {
     $('.presentation-card').load('fragments/presentation-card.html');
 }
@@ -7,7 +18,17 @@ function initNavbar() {
     $('.navbar').load('fragments/nav-bar.html');
 }
 
+function selectSection(section) {
+    selectedSection = section;
+    if(selectedSection !== undefined) initSelectedSection();
+}
+
+function initSelectedSection() {
+    $('.section').load('fragments/sections/' + selectedSection + '.html');
+}
+
 $(document).ready(function() {
     initPresentationCard();
     initNavbar();
+    selectSection(SECTIONS.INTRODUCTION);
   });
