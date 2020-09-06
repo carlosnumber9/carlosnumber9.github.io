b25373db33a103f179e66394c039739a713e9947
carlosnumber9 <charlie_theboss023@hotmail.com>
2020-09-06 19:21:14 +0200

    Height animations on section selection

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 
+
+/****************************** CONSTANTS & VARIABLES *********************/
+
 var SECTIONS = {
     INTRODUCTION: 'introduction',
     CAREER_GOALS: 'career-goals',
@@ -10,6 +13,76 @@ var SECTIONS = {
 
 var selectedSection;
 
+
+
+
+
+
+
+/****************************** FUNCTIONS *********************/
+
+function selectSection(section) {
+    if(selectedSection === undefined) {
+        selectedSection = section;
+        if(selectedSection !== undefined) initSelectedSection();
+    }
+    else {
+        $('.section').children().fadeOut().promise().done(() => {
+            selectedSection = section;
+            if(selectedSection !== undefined) initSelectedSection();
+        });
+    }
+}
+
+function getTotalHeight(selector) {
+    var total = 0;
+    selector.children().each(function(child){
+        total += $(this).outerHeight() + 
+            getVerticalMargin($(this)) + 
+            getPadding($(this));
+    });
+    return total;
+}
+
+function getNewHeightForSection(sectionSelector) {
+    return getPadding(sectionSelector) + 
+    getTotalHeight(sectionSelector) +
+    getVerticalMargin(sectionSelector);
+}
+
+function getPadding(selector) {
+    return parseInt(selector.css('padding').replace('px', ''))*2;
+}
+
+function getVerticalMargin(selector) {
+    return parseInt(selector.css('margin-top').replace('px', '')) + parseInt(selector.css('margin-bottom').replace('px', ''));
+}
+
+
+
+
+
+/****************************** INIT METHODS *********************/
+
+function initResizeHandlers() {
+    $(window).resize(function() {
+        var section = $('.section');
+        section.css('height', getNewHeightForSection(section));
+      });
+}
+
+function initSelectedSection() {
+    var section = $('.section');
+    section.load('fragments/sections/' + selectedSection + '.html', () => {
+        var height = section.outerHeight();
+        section.animate(
+            { height: getNewHeightForSection(section) }, 
+            200, 
+            () => { section.children().fadeIn(150); }
+        );
+    });
+}
+
 function initPresentationCard() {
     $('.presentation-card').load('fragments/presentation-card.html');
 }
@@ -18,17 +91,10 @@ function initNavbar() {
     $('.navbar').load('fragments/nav-bar.html');
 }
 
-function selectSection(section) {
-    selectedSection = section;
-    if(selectedSection !== undefined) initSelectedSection();
-}
-
-function initSelectedSection() {
-    $('.section').load('fragments/sections/' + selectedSection + '.html');
-}
 
 $(document).ready(function() {
     initPresentationCard();
     initNavbar();
     selectSection(SECTIONS.INTRODUCTION);
+    initResizeHandlers();
   });
