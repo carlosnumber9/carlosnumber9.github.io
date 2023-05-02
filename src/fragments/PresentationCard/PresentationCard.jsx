c037789f02decabdf6412f4125c51acff19fe17c
Carlos Pinedo <carlos.pinedo@devo.com>
2023-05-02 07:38:36 +0200

    feat: Added age tooltip

--- src/fragments/PresentationCard/PresentationCard.jsx
+++ src/fragments/PresentationCard/PresentationCard.jsx
@@ -9,11 +9,14 @@ import {
   faMobile,
   faSeedling,
 } from '@fortawesome/free-solid-svg-icons';
+import { Tooltip } from '@mui/material';
 
 export const PresentationCard = () => {
   const getAgeInSeconds = () =>
-    differenceInSeconds(new Date(), new Date(1995, 6, 21)).toLocaleString();
+    differenceInSeconds(new Date(), new Date(1995, 6, 21));
   const [age, setAge] = useState(getAgeInSeconds());
+
+  const getAgeInYears = (ageInSeconds) => Math.floor(ageInSeconds / (60 * 60 * 24 * 365.25));
 
   useEffect(() => {
     const interval = setInterval(() => setAge(getAgeInSeconds()), 1000);
@@ -22,45 +25,60 @@ export const PresentationCard = () => {
   }, []);
 
   return (
-    <StyledCard>
-      <div className="card-column first-column">
-        <FontAwesomeIcon icon={faSeedling} size={'lg'} id="age-icon" />
-        <span className="card-text" id="age-text">
-          {`${age} seconds old`}
-        </span>
-        <div className="card-hr" id="first-hr">
-          <hr />
-        </div>
-        <FontAwesomeIcon icon={faMobile} size={'lg'} id="phone-number-icon" />
-        <span className="card-text" id="phone-number-text">
-          <a href="tel:686637099">686637099</a>
-        </span>
-        <div className="card-hr" id="second-hr">
-          <hr />
-        </div>
-        <FontAwesomeIcon icon={faInbox} size={'lg'} id="email-icon" />
-        <span id="email-text">
-          <a href="mailto:cpinedocsb@gmail.com">
-            <span className="card-text">cpinedocsb@gmail.com</span>
-          </a>
-        </span>
-      </div>
-      <div className="card-column second-column">
-        <span id="linkedin-button" className="card-element">
-          <a
-            href="https://www.linkedin.com/in/carlos-pinedo-s%C3%A1nchez-b661a7130/"
-            target="blank"
+    <>
+      <StyledCard>
+        <div className="card-column first-column">
+          <FontAwesomeIcon icon={faSeedling} size={'lg'} id="age-icon" />
+          <Tooltip
+            placement="top"
+            title={`Yep, that is ${getAgeInYears(age)} years old.`}
           >
-            <FontAwesomeIcon icon={faLinkedin} size={'3x'} className={'card-icon'} />
-          </a>
-        </span>
-        <span id="github-logo">
-          <a href="https://www.github.com/carlosnumber9" target="blank">
-            <FontAwesomeIcon icon={faGithub} size={'3x'} className={'card-icon'} />
-          </a>
-        </span>
-      </div>
-    </StyledCard>
+            <span className="card-text" id="age-text">
+              {`${age.toLocaleString()} seconds old`}
+            </span>
+          </Tooltip>
+          <div className="card-hr" id="first-hr">
+            <hr />
+          </div>
+          <FontAwesomeIcon icon={faMobile} size={'lg'} id="phone-number-icon" />
+          <span className="card-text" id="phone-number-text">
+            <a href="tel:686637099">686637099</a>
+          </span>
+          <div className="card-hr" id="second-hr">
+            <hr />
+          </div>
+          <FontAwesomeIcon icon={faInbox} size={'lg'} id="email-icon" />
+          <span id="email-text">
+            <a href="mailto:cpinedocsb@gmail.com">
+              <span className="card-text">cpinedocsb@gmail.com</span>
+            </a>
+          </span>
+        </div>
+        <div className="card-column second-column">
+          <span id="linkedin-button" className="card-element">
+            <a
+              href="https://www.linkedin.com/in/carlos-pinedo-s%C3%A1nchez-b661a7130/"
+              target="blank"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                size={'3x'}
+                className={'card-icon'}
+              />
+            </a>
+          </span>
+          <span id="github-logo">
+            <a href="https://www.github.com/carlosnumber9" target="blank">
+              <FontAwesomeIcon
+                icon={faGithub}
+                size={'3x'}
+                className={'card-icon'}
+              />
+            </a>
+          </span>
+        </div>
+      </StyledCard>
+    </>
   );
 };
 
