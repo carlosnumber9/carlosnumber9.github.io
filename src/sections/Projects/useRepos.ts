e9135320b5de14218cf1d1258a9f53205bfa5422
carlosnumber9 <charlie_theboss023@hotmail.com>
2024-03-23 10:54:13 +0100

    build: Changed to Netlify deployment

--- src/sections/Projects/useRepos.ts
+++ src/sections/Projects/useRepos.ts
@@ -7,35 +7,25 @@ export const useRepos = () => {
 
   const fetchUserRepos = async () => {
     setLoading(true);
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/users/${import.meta.env.VITE_USERNAME}/repos`,
-        {
-          headers: {
-            Authorization: `token ${import.meta.env.VITE_TOKEN}`,
-          },
-        },
-      );
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      setRepos(data.filter((repo: any) => repo.topics.includes('showcase')));
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching GitHub repositories:', error);
-      throw error;
-    }
+    await fetch('/.netlify/functions/github')
+      .then(async (response) => {
+        const data = await response.json();
+        setRepos(data.filter((repo: any) => repo.topics.includes('showcase')));
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => setLoading(false));
   };
 
   useEffect(() => {
-     if (!loading) {
-       fetchUserRepos();
-     }
+    if (!loading) {
+      fetchUserRepos();
+    }
   }, [loading]);
- 
+
   if (loading) {
-     throw fetchUserRepos(); // Throw the promise to be caught by Suspense
+    throw fetchUserRepos(); // Throw the promise to be caught by Suspense
   }
 
   return { loading, repos };
