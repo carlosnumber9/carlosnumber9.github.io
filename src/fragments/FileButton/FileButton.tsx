d425db60e5d7684b69abcba7c0c6745037f5b5a7
carlosnumber9 <charlie_theboss023@hotmail.com>
2024-04-01 11:32:47 +0200

    feat: PDF tooltip

--- src/fragments/FileButton/FileButton.tsx
+++ src/fragments/FileButton/FileButton.tsx
@@ -1,11 +1,14 @@
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { StyledFileButton } from './StyledFileButton';
+import { Tooltip } from '@mui/material';
 
 export const FileButton: React.FC = () => (
-  <StyledFileButton>
-    <a download="" href="/CV8_CarlosPinedoSanchez_EN.pdf">
-      <FontAwesomeIcon icon={faFilePdf} size="xl" className="file-icon" />
-    </a>
-  </StyledFileButton>
+  <Tooltip placement="left" title={'Download my CV in PDF format'}>
+    <StyledFileButton>
+      <a download="" href="/CV8_CarlosPinedoSanchez_EN.pdf">
+        <FontAwesomeIcon icon={faFilePdf} size="xl" className="file-icon" />
+      </a>
+    </StyledFileButton>
+  </Tooltip>
 );
