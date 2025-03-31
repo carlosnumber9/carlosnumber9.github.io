411090440c9cd783cf265adf13d27d1a545af166
Carlos. <cpinedocsb@gmail.com>
2025-03-31 18:26:00 +0000

    fix: Responsiveness

--- src/MainColumn.ts
+++ src/MainColumn.ts
@@ -1,6 +1,25 @@
 import styled from 'styled-components';
 
 export const MainColumn = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  font-weight: 100;
+  -webkit-font-smoothing: antialiased;
+
+  -webkit-box-shadow: 3px 0px 5px 0px rgba(156, 156, 156, 1),
+    -3px 0px 5px 0px rgba(156, 156, 156, 1);
+  -moz-box-shadow: 3px 0px 5px 0px rgba(156, 156, 156, 1),
+    -3px 0px 5px 0px rgba(156, 156, 156, 1);
+  box-shadow: 3px 0px 5px 0px rgba(156, 156, 156, 1),
+    -3px 0px 5px 0px rgba(156, 156, 156, 1);
+
+  font-family: 'Oxygen', sans-serif;
+
   @media only screen and (min-width: 980px) {
     width: 980px;
   }
@@ -12,29 +31,5 @@ export const MainColumn = styled.div`
   @media only screen and (max-width: 600px) {
     width: 100vw;
   }
-  
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
 
-  min-height: 100vh;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-
-  font-weight: 100;
-  -webkit-font-smoothing: antialiased;
-  
-  -webkit-box-shadow: 3px 0px 5px 0px rgba(156, 156, 156, 1),
-    -3px 0px 5px 0px rgba(156, 156, 156, 1);
-  -moz-box-shadow: 3px 0px 5px 0px rgba(156, 156, 156, 1),
-    -3px 0px 5px 0px rgba(156, 156, 156, 1);
-  box-shadow: 3px 0px 5px 0px rgba(156, 156, 156, 1),
-    -3px 0px 5px 0px rgba(156, 156, 156, 1);
-
-  font-family: 'Oxygen', sans-serif;
 `;
