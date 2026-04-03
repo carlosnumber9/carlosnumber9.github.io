01b0879a3d65512dc82e3f59821b5d1c4239a40b
Carlos Pinedo <cpinedocsb@gmail.com>
2026-04-03 17:10:13 +0200

    refactor: Brought back repo to life

--- src/fragments/FileButton/FileButton.tsx
+++ src/fragments/FileButton/FileButton.tsx
@@ -1,14 +1,11 @@
 import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { StyledFileButton } from './StyledFileButton';
-import { Tooltip } from '@mui/material';
 
 export const FileButton: React.FC = () => (
-  <Tooltip placement="left" title={'Download my CV in PDF format'}>
-    <StyledFileButton>
-      <a download="" href="/CV10_CarlosPinedoSanchez_EN.pdf">
-        <FontAwesomeIcon icon={faFileAlt} size="xl" className="file-icon" />
-      </a>
-    </StyledFileButton>
-  </Tooltip>
+  <StyledFileButton title="Download my CV in PDF format">
+    <a download="" href="/CV10_CarlosPinedoSanchez_EN.pdf">
+      <FontAwesomeIcon icon={faFileAlt} size="xl" className="file-icon" />
+    </a>
+  </StyledFileButton>
 );
